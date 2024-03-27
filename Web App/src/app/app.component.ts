82f29b1f627c9e2601ce50b6c404125fcdb07637
heli1607 <helishah1010@gamil.com>
2024-03-27 15:55:17 +0530

    DID IT!

--- Web App/src/app/app.component.ts
+++ Web App/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   title = 'w2m';
-  welcome="Welcome to"+"<br>"+"WAY 2 MENU";
+  welcome="Welcome to \n WAY 2 MENU";
   constructor(private router: Router) { }
 
   ngOnInit(): void {
