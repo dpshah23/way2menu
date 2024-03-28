15380fc53ab6f872a3df90a424a0cf0e7ba85c91
heli1607 <helishah1010@gamil.com>
2024-03-28 21:34:12 +0530

    cart

--- Web App/src/app/app.module.ts
+++ Web App/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
