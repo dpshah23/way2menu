ccf5885836513ae070684c62c565c2fd8f1f66c9
heli <helishah1010@gmail.com>
2024-03-30 15:17:14 +0530

    api call

--- signup/src/app/data.service.ts
+++ signup/src/app/data.service.ts
@@ -11,6 +11,6 @@ export class DataService {
   constructor() { }
   send(name:any,logo:any,address:any,owner_name:any,gst_number:any,owner_age:any,owner_gender:any,owner_number:any,owner_email:any,account_password:any){
     const data={name:name,logo:logo,address:address,owner_name:owner_name,gst_number:gst_number,owner_age:owner_age,owner_gender:owner_gender,owner_number:owner_number,owner_email:owner_email,account_password:account_password};
-    return this.http.post('http://127.0.0.1:8000/login/',data);
+    return this.http.post('http://127.0.0.1:8000/addrestaurant/',data);
   }
 }
