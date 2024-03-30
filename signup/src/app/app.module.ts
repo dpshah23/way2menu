db3fa31bf1be6a881d1c9510a163ada4996cffee
heli <helishah1010@gmail.com>
2024-03-30 15:46:13 +0530

    firebase

--- signup/src/app/app.module.ts
+++ signup/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import {AngularFireModule } from '@angular/fire/compat';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +16,11 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"way2menu-e0407","appId":"1:542346807588:web:5b5348183394ef55894bb1","storageBucket":"way2menu-e0407.appspot.com","apiKey":"AIzaSyDokqa_V86tcOIJpJ1EgIXrgaSlSmP3tYE","authDomain":"way2menu-e0407.firebaseapp.com","messagingSenderId":"542346807588"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
