c7fae03e061ca64bb6693928339e5393013e175c
heli <helishah1010@gmail.com>
2024-04-06 15:07:01 +0530

    signup routing

--- Web App/src/app/app.component.ts
+++ Web App/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import { Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { DataService } from '../app/data.service';
 @Component({
   selector: 'app-root',
@@ -10,22 +10,25 @@ export class AppComponent implements OnInit {
   title = 'w2m';
   email='';
   password='';
-  constructor(private dataService: DataService) { }
+  signup1:boolean=false;
+  hide:boolean=false;
+  constructor(private dataService: DataService, private router:Router) { }
   ngOnInit(): void {}
 
   send(){
     this.dataService.send(this.email,this.password).subscribe((result)=>
     (console.warn(result)))
   }
+  signup(){
+    this.signup1 = true;
+    this.hide=!this.hide;
+    this.router.navigate(['/signup']);
+  }
+  //routing functions if needed
   /*ngOnInit(): void {
     const dataToSend = {'email':this.email,'password':this.password};
     this.dataService.sendData(dataToSend).subscribe(response => {
       console.log(response); // Handle the response from the API
     });
   }*/
-  /*constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
-  //routing functions if needed*/
 }
